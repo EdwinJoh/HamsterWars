6299fcc5f2d92f29b56d896972a502c52f4acb57
Edwin johansson <edwinjohansson95@icloud.com>
2022-04-27 14:25:48 +0200

    some small fixes

---
+++ HamsterWars/Services/GetResults.cs
@@ -0,0 +1,35 @@
+﻿using DataAccessLibrary;
+
+namespace HamsterWars.Services
+{
+    public static class GetResults
+    {
+
+        public static HamsterModel GetWinner(int id, List<HamsterModel> hamsterList)
+        {
+            if (hamsterList.Any(i => i.Id == id))
+            {
+                return hamsterList.Where(s => s.Id == id).Single();
+
+            }
+            else
+            {
+                return new HamsterModel
+                {
+                    Name = "Unknown"
+                };
+            }
+        }
+        public static HamsterModel GetLoser(int id, List<HamsterModel> hamsterList)
+        {
+            if (hamsterList.Any(i => i.Id == id))
+            {
+               return hamsterList.Where(c => c.Id == id).Single();
+            }
+            else
+            {
+                return new HamsterModel { Name ="Unknown" };
+            }
+        }
+    }
+}
