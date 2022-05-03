1231a1609b1fabe18d7ba798a095dd6a6bd83494
Edwin johansson <edwinjohansson95@icloud.com>
2022-05-03 14:59:41 +0200

    Created some components and cleaned up the code , done

---
+++ Application/Events/AddHamsterToList.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Events
+{
+    public class AddHamsterToList
+    {
+        public static void AddHamster(HamsterModel hamster, List<HamsterModel> hamsterList)
+        {
+            hamsterList.Add(hamster);
+        }
+    }
+}
---
+++ Application/Events/BattleEvent.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLibrary;
+
+namespace Application.Events
+{
+    public class BattleEvent
+    {
+        public static void BattleUpdate(HamsterModel Winner, HamsterModel Loser)
+        {
+            Winner.Games++;
+            Winner.Wins++;
+            Loser.Games++;
+            Loser.Defeats++;
+        }
+    }
+}
